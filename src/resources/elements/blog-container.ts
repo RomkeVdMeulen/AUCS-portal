31f07c44eca62c300ce215abb3fabad5302b6815
Cristián Ormazábal (Pasamonte) <cristian.ormazabal@pasamonte.cl>
2018-01-11 10:16:37 -0300

    Implement the custom page to host Guides #5

--- src/resources/elements/blog-container.ts
+++ src/resources/elements/blog-container.ts
@@ -2,7 +2,7 @@ import {Router} from 'aurelia-router';
 import {inject} from 'aurelia-framework';
 
 @inject(Router)
-export class Docs {
+export class BlogContainer {
   router: Router;
   constructor(router: Router) {
     this.router = router;
