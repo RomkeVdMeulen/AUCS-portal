dada678f3f747358f78c2cf87f9c32b63951d53a
Cristián Ormazábal (Pasamonte) <cristian.ormazabal@pasamonte.cl>
2018-01-14 00:59:51 -0300

    mejora navegación

--- src/guides/guide-bar.ts
+++ src/guides/guide-bar.ts
@@ -1,5 +1,19 @@
-import {bindable} from 'aurelia-framework';
+import { Router } from 'aurelia-router';
+import { inject } from 'aurelia-framework';
+import { bindable } from 'aurelia-framework';
 
+@inject(Router)
 export class GuideBar {
-  @bindable chapters;
+  @bindable atRoot;
+  @bindable title;
+  @bindable items;
+  router: Router;
+  constructor(router: Router) {
+    this.router = router;
+  }
+
+  itemClicked(params: any): void {
+    const href = params.target.href as string;
+    const target = href.substr(href.indexOf('content/')).replace('content/', '');
+  }
 }
